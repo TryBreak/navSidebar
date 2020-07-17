4579f73955fc51591f03bce5b499117cdf8013bc
墨七 <meichangliang@outlook.com>
2020-07-17 18:25:38 +0800

    函数机制改版

--- src/navSidebar.js
+++ src/navSidebar.js
@@ -10,174 +10,182 @@ function navSidebarMethod(status_str, callback) {
       inset: Boolean,
     })
   */
-
-  var sidebarTypeList = ["inset", "fixed"];
-
   // 存在对应 class 则表示是对应状态 , 默认都不存在
-  var status_list_class = {
-    inset_off: "nav_sidebar-narrow",
-    inset_on: "nav_sidebar-wide",
-    fixed_off: "nav_sidebar-hidden",
-    fixed_on: "nav_sidebar-show",
-    boxCSSMedia: "nav-sidebar",
-    boxJSMedia: "js-nav-sidebar",
-  };
-
-  var transitionTime = 200;
-  var navSidebar_w = 240;
-  var navSidebar_s_w = 72;
-
-  var type;
-  var fixedElm = null;
-  var insetElm = null;
-  var isInsetOff;
-  var isInsetOn;
-  var isInsetDisabled;
-  var isFixedOff;
-  var isFixedOn;
 
   var utils = {
+    sidebarTypeList: ["inset", "fixed"],
+    transitionTime: 200,
+
+    navSidebar_w: 240,
+    navSidebar_s_w: 72,
+
+    type: null,
+
+    fixedElm: null,
+    insetElm: null,
+
+    isInsetOn: null,
+    isInsetDisabled: null,
+
+    isFixedOn: null,
+
+    status_list_class: {
+      inset_off: "nav_sidebar-narrow",
+      inset_on: "nav_sidebar-wide",
+      fixed_off: "nav_sidebar-hidden",
+      fixed_on: "nav_sidebar-show",
+      boxCSSMedia: "nav-sidebar",
+      boxJSMedia: "js-nav-sidebar",
+    },
     readType: function () {
-      if (status_str === sidebarTypeList[0]) {
-        type = sidebarTypeList[0];
+      if (status_str === this.sidebarTypeList[0]) {
+        this.type = this.sidebarTypeList[0];
       }
-      if (status_str === sidebarTypeList[1]) {
-        type = sidebarTypeList[1];
+      if (status_str === this.sidebarTypeList[1]) {
+        this.type = this.sidebarTypeList[1];
       }
       this.getElm();
-      return type;
+      return this.type;
     },
     getElm: function () {
-      insetElm = document.getElementById("js-nav-sidebar-inset");
-      fixedElm = document.getElementById("js-nav-sidebar-fixed");
-      insetElmStyle = window.getComputedStyle(insetElm);
-      isInsetDisabled = insetElmStyle.display === "none";
+      this.insetElm = document.getElementById("js-nav-sidebar-inset");
+      this.fixedElm = document.getElementById("js-nav-sidebar-fixed");
+      insetElmStyle = window.getComputedStyle(this.insetElm);
+      this.isInsetDisabled = insetElmStyle.display === "none";
       return {
-        fixedElm: fixedElm,
-        insetElm: insetElm,
+        fixedElm: this.fixedElm,
+        insetElm: this.insetElm,
       };
     },
     readStatus: function () {
       //开关状态不再根据类名读取，根据宽度读取
-      if (isInsetDisabled) {
+      console.log("111", this.isInsetOn);
+      if (this.isInsetDisabled) {
         // Disabled
       } else {
-        console.log("显示");
-        var insetElmStyle = window.getComputedStyle(insetElm);
+        var insetElmStyle = window.getComputedStyle(this.insetElm);
         var insetWidth = parseInt(insetElmStyle.width);
-        if (insetWidth > navSidebar_s_w) {
-          isInsetOn = true;
+        if (insetWidth > this.navSidebar_s_w) {
+          this.isInsetOn = true;
+        } else {
+          this.isInsetOn = false;
         }
-        isInsetOff = !isInsetOn;
       }
 
-      isFixedOff = !fixedElm.classList.contains(status_list_class.fixed_on);
-      isFixedOn = !fixedElm.classList.contains(status_list_class.fixed_off);
+      var fixedOff = !this.fixedElm.classList.contains(
+        this.status_list_class.fixed_on
+      );
+      var fixedOn = !this.fixedElm.classList.contains(
+        this.status_list_class.fixed_off
+      );
+
       //浮动式默认为关闭状态
-      if (isFixedOff && isFixedOn) {
-        isFixedOn = false;
+      if (fixedOff && fixedOn) {
+        this.isFixedOn = false;
+      } else {
+        this.isFixedOn = true;
       }
 
       return {
-        isInsetOn: isInsetOn,
-        isFixedOn: isFixedOn,
+        isInsetOn: this.isInsetOn,
+        isFixedOn: this.isFixedOn,
       };
     },
     disabledCSSMedia: function () {
       var boxElm = document.getElementById("js-nav-sidebar");
-      boxElm.classList.remove(status_list_class.boxCSSMedia);
-      boxElm.classList.add(status_list_class.boxJSMedia);
+      boxElm.classList.remove(this.status_list_class.boxCSSMedia);
+      boxElm.classList.add(this.status_list_class.boxJSMedia);
     },
     inset_off: function () {
-      if (!insetElm) {
+      if (!this.insetElm) {
         return null;
       }
       this.disabledCSSMedia();
-      insetElm.classList.remove(status_list_class.inset_on);
-      insetElm.classList.add(status_list_class.inset_off);
-      return inform();
+      this.insetElm.classList.remove(this.status_list_class.inset_on);
+      this.insetElm.classList.add(this.status_list_class.inset_off);
+      return this.inform();
     },
     inset_on: function () {
-      if (!insetElm) {
+      if (!this.insetElm) {
         return null;
       }
       this.disabledCSSMedia();
-      insetElm.classList.remove(status_list_class.inset_off);
-      insetElm.classList.add(status_list_class.inset_on);
-      return inform();
+      this.insetElm.classList.remove(this.status_list_class.inset_off);
+      this.insetElm.classList.add(this.status_list_class.inset_on);
+      return this.inform();
     },
     fixed_off: function () {
-      fixedElm.classList.remove(status_list_class.fixed_on);
-      fixedElm.classList.add(status_list_class.fixed_off);
+      this.fixedElm.classList.remove(this.status_list_class.fixed_on);
+      this.fixedElm.classList.add(this.status_list_class.fixed_off);
       setTimeout(function () {
-        fixedElm.style.display = "none";
-        inform();
-      }, transitionTime);
+        this.fixedElm.style.display = "none";
+        this.inform();
+      }, this.transitionTime);
       return "fixed_off";
     },
     fixed_on: function () {
-      fixedElm.style.display = "block";
+      this.fixedElm.style.display = "block";
       setTimeout(function () {
-        fixedElm.classList.remove(status_list_class.fixed_off);
-        fixedElm.classList.add(status_list_class.fixed_on);
-        inform();
+        this.fixedElm.classList.remove(this.status_list_class.fixed_off);
+        this.fixedElm.classList.add(this.status_list_class.fixed_on);
+        this.inform();
       });
       return "fixed_on";
     },
-    htmlOn: function () {
+    mainOn: function () {
       var mainElm = document.getElementById("js-nav-sidebar-main");
-      mainElm.style.paddingLeft = navSidebar_w + "px";
+      mainElm.style.paddingLeft = this.navSidebar_w + "px";
       return mainElm.style.paddingLeft;
     },
-    htmlOff: function () {
+    mainOff: function () {
       var mainElm = document.getElementById("js-nav-sidebar-main");
-      mainElm.style.paddingLeft = navSidebar_s_w + "px";
+      mainElm.style.paddingLeft = this.navSidebar_s_w + "px";
       return mainElm.style.paddingLeft;
+    },
+    inform: function () {
+      //读取当前状态并输出
+      utils.readStatus();
+      var param = {
+        fixed: this.isFixedOn,
+        inset: this.isInsetOn,
+      };
+      callback && callback(param);
+      console.log(param);
+      return param;
     },
   };
 
-  //侦测当行类型
-  utils.readType();
-
-  // 读取当前状态
-  utils.readStatus();
-
-  // 执行切换
-  if (isInsetDisabled) {
-    type = sidebarTypeList[1];
+  if (status_str) {
+    //侦测当行类型
+    utils.readType();
+    // 读取当前状态
+    utils.readStatus();
   }
 
-  if (type === sidebarTypeList[0]) {
-    if (isInsetOff) {
-      utils.inset_on();
-      utils.htmlOn();
-    } else {
+  // 执行切换
+  if (utils.isInsetDisabled) {
+    utils.type = utils.sidebarTypeList[1];
+  }
+
+  if (utils.type === utils.sidebarTypeList[0]) {
+    if (utils.isInsetOn) {
       utils.inset_off();
-      utils.htmlOff();
+      utils.mainOff();
+    } else {
+      utils.inset_on();
+      utils.mainOn();
     }
   }
 
-  if (type === sidebarTypeList[1]) {
-    if (isFixedOff) {
-      utils.fixed_on();
-    } else {
+  if (utils.type === utils.sidebarTypeList[1]) {
+    if (utils.isFixedOn) {
       utils.fixed_off();
+    } else {
+      utils.fixed_on();
     }
   }
 
   return utils;
-
-  //通知
-  function inform() {
-    //读取当前状态并输出
-    utils.readStatus();
-    var param = {
-      fixed: isFixedOn,
-      inset: isInsetOn,
-    };
-    callback && callback(param);
-    return param;
-  }
 }
 
 // 切换开关
@@ -185,38 +193,24 @@ function navSidebarSwitch() {
   var winW = document.body.clientWidth;
   var window_w = 1320; //宽窄模式自动切换临界点
   if (winW - window_w > 0) {
-    console.log("嵌入式切换开始");
     navSidebarMethod("inset", function (param) {
       global_navSidebar_inset = param.inset;
     });
   } else {
-    console.log("浮动式切换开始");
     navSidebarMethod("fixed");
   }
-  // 默认嵌入式展开收起
-  // 如果宽度小于 1320 , 则默认为浮动式
-  // 如果不存在嵌入式，则为浮动式展开收起
 }
 
 // $(function(){
-// navSidebarInitialize();
-// function navSidebarInitialize() {
-//   var navSidebarInitialize_main = function main() {
-//     var winW = document.body.clientWidth;
-//     var window_w = 1320; //宽窄模式自动切换临界点
-//     var method = navSidebarMethod(null);
-//     if (!global_navSidebar_inset) {
-//       return;
-//     }
-//     if (winW - window_w > 0) {
-//       method.inset_on();
-//       method.htmlOn();
-//     } else {
-//       method.inset_off();
-//       method.htmlOff();
-//     }
-//   };
-//   navSidebarInitialize_main();
-//   window.onresize = navSidebarInitialize_main;
-// }
+navSidebarInitialize();
+function navSidebarInitialize() {
+  var navSidebarInitialize_main = function () {
+    var winW = document.body.clientWidth;
+    var window_w = 1320; //宽窄模式自动切换临界点
+    var method = navSidebarMethod(null);
+  };
+  window.onresize = function (params) {
+    navSidebarInitialize_main();
+  };
+}
 // })
